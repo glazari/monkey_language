20210afc7b2f49ae70c4213727945605c559194f
Guilherme Lazari <guilherme.lazari@wildlifestudios.com>
2021-03-25 23:19:06 -0300

    Add lexer

---
+++ token/token.go
@@ -0,0 +1,46 @@
+package token
+
+type TokenType string
+
+type Token struct {
+	Type    TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF     = "EOF"
+
+	// Identifiers  literals
+	IDENT = "IDENT"
+	INT   = "INT"
+
+	// Operators
+	ASSIGN = "ASSIGN"
+	PLUS   = "PLUS"
+
+	// Delimiters
+	COMMA     = "COMMA"
+	SEMICOLON = "SEMICOLON"
+
+	LPAREN = "LPARAN"
+	RPAREN = "RPAREN"
+	LBRACE = "LBRACE"
+	RBRACE = "RBRACE"
+
+	// Keywords
+	FUNCTION = "FUNCTION"
+	LET      = "LET"
+)
+
+var keywords = map[string]TokenType{
+	"fn":  FUNCTION,
+	"let": LET,
+}
+
+func LookupIdent(ident string) TokenType {
+	if tok, ok := keywords[ident]; ok {
+		return tok
+	}
+	return IDENT
+}
