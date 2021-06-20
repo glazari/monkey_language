78c393b52dbe8adfd5636b6d4a1a260a1ec56324
Guilherme Lazari <guilherme.lazari@wildlifestudios.com>
2021-06-20 14:07:10 -0300

    Add return statement evaluation

--- evaluator/evaluator_test.go
+++ evaluator/evaluator_test.go
@@ -98,6 +98,32 @@ func TestIfElseExpressions(t *testing.T) {
 	}
 }
 
+func TestReturnStatement(t *testing.T) {
+	tests := []struct {
+		input    string
+		expected int64
+	}{
+		{"return 10;", 10},
+		{"return 10;", 10},
+		{"return 2 * 5;", 10},
+		{"9; return 2 * 5;", 10},
+		{`
+		if (10 > 1) {
+			if (10 > 1) {
+				return 10;
+			}
+			return 1;
+		}`, 10},
+	}
+
+	for _, test := range tests {
+		t.Run(test.input, func(t *testing.T) {
+			evaluated := testEval(test.input)
+			testIntegerObject(t, evaluated, test.expected)
+		})
+	}
+}
+
 func TestBangOperator(t *testing.T) {
 	tests := []struct {
 		input    string
--- object/object.go
+++ object/object.go
@@ -5,9 +5,10 @@ import "fmt"
 type ObjectType string
 
 const (
-	INTEGER_OBJ = "INTEGER"
-	BOOLEAN_OBJ = "BOOLEAN"
-	NULL_OBJ    = "NULL"
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NULL_OBJ         = "NULL"
+	RETURN_VALUE_OBJ = "RETURN_VALUE"
 )
 
 type Object interface {
@@ -15,21 +16,21 @@ type Object interface {
 	Inspect() string
 }
 
-type Integer struct {
-	Value int64
-}
+type Integer struct{ Value int64 }
+type Boolean struct{ Value bool }
+type Null struct{}
+type ReturnValue struct{ Value Object }
+
+// Types implementing the Object interface
 
 func (i *Integer) Inspect() string  { return fmt.Sprintf("%d", i.Value) }
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
 
-type Boolean struct {
-	Value bool
-}
-
 func (b *Boolean) Inspect() string  { return fmt.Sprintf("%t", b.Value) }
 func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
 
-type Null struct{}
-
 func (n *Null) Inspect() string  { return "null" }
 func (n *Null) Type() ObjectType { return NULL_OBJ }
+
+func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
