515f0b5811cb42cb388aeca6db0f214d86cbd8e5
Guilherme Lazari <guilherme.lazari@wildlifestudios.com>
2021-06-20 14:52:20 -0300

    Add error handling

--- object/object.go
+++ object/object.go
@@ -9,6 +9,7 @@ const (
 	BOOLEAN_OBJ      = "BOOLEAN"
 	NULL_OBJ         = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
+	ERROR_OBJ        = "ERROR"
 )
 
 type Object interface {
@@ -20,6 +21,7 @@ type Integer struct{ Value int64 }
 type Boolean struct{ Value bool }
 type Null struct{}
 type ReturnValue struct{ Value Object }
+type Error struct{ Message string }
 
 // Types implementing the Object interface
 
@@ -34,3 +36,6 @@ func (n *Null) Type() ObjectType { return NULL_OBJ }
 
 func (rv *ReturnValue) Inspect() string  { return rv.Value.Inspect() }
 func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+
+func (e *Error) Inspect() string  { return "ERROR: " + e.Message }
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
